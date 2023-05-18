d17d553c714a533b72523b9c14cf375214c7f7cf
MiguelBraga2 <mgdsbraga@gmail.com>
2023-05-18 11:18:56 +0100

    Writer added in .hpp

--- code/shared/IO.hpp
+++ code/shared/IO.hpp
@@ -5,7 +5,7 @@
 #include "triangle.hpp"
 
 void writer(const string& fileName, vector<unsigned int> indexes, vector<float> vertices);
-void writer(const string& fileName, vector<float> vertices, vector<unsigned int> verticesIndexes, vector<float> normals, vector<unsigned int> normalsIndexes);
+void writer(const string& fileName, vector<float> vertices, vector<unsigned int> verticesIndexes, vector<float> normals, vector<unsigned int> normalsIndexes, vector<float> textCoord, vector<unsigned int> textIndexes);
 vector<float> reader(const string& fileName, vector<unsigned int>* indexes);
 vector<float> reader(const string& fileName, vector<unsigned int>* indexes, vector<float>* normals, vector<float>* textCoords);
 void crossProduct(float vectAX, float vectAY, float vectAZ, float vectBX, float vectBY, float vectBZ, float cross_P[]);
